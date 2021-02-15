6df77001dceb921e32d9aba552956130cd864eb8
CrispyMoon <kevinnolasco811@gmail.com>
2021-02-15 10:39:57 -0800

    Changing index.php

--- index.php
+++ index.php
@@ -4,23 +4,15 @@
 $con = mysqli_connect("172.19.0.2","root","trucorp","Trucorp");
 
 if($con->connect_error){
-	die("Koneksi gagal: " .$con->connect_error);
+        die("Koneksi gagal: " .$con->connect_error);
 }
 
-$tbl = "SELECT ID, Nama, Kantor FROM users";
+$tbl = "SELECT * FROM users";
 $res = $con->query($tbl);
 
-if($res->num_rows > 0){
-	$idx = 1;
-	while($row = $res->fetch_assoc()){
-		echo "Data " . $idx . "<br>";
-		echo "----------------------------------------------" . "<br>";
-		echo "ID : " . $row["ID"] . "<br>";
-		echo "Nama : " . $row["Nama"] . "<br>";
-		echo "Kantor : " . $row["Kantor"] . "<br><br>";
-		$idx++;
-	}
-}
+$counts = mysqli_num_rows($$res);
+
+echo "Pada database Trucorp, terdapat " . $counts . " user(s)"
 
 mysqli_close($con);
 
